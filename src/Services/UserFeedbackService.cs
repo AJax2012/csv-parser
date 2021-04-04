06c740ea61323e5fb5171eae2d7a6fbf980fbbb8
Adam Gardner <adga2012@gmail.com>
2021-04-04 12:14:48 -0600

    update name of IConsole in UserFeedbackService

--- src/Services/UserFeedbackService.cs
+++ src/Services/UserFeedbackService.cs
@@ -6,12 +6,12 @@ namespace GardnerCsvParser.Services
     public class UserFeedbackService : IUserFeedbackService
     {
         private readonly IFileService _fileService;
-        private readonly IConsole _consoleRetriever;
+        private readonly IConsole _console;
 
-        public UserFeedbackService(IFileService fileService, IConsole consoleRetriever)
+        public UserFeedbackService(IFileService fileService, IConsole console)
         {
             _fileService = fileService;
-            _consoleRetriever = consoleRetriever;
+            _console = console;
         }
 
         public string GetInputFileLocation()
@@ -21,12 +21,12 @@ namespace GardnerCsvParser.Services
 
             while (string.IsNullOrEmpty(response))
             {
-                _consoleRetriever.WriteTextForUser("Please type the location of the file you'd like to parse.");
-                response = _consoleRetriever.GetTextFromUser(hasFailed);
+                _console.WriteTextForUser("Please type the location of the file you'd like to parse.");
+                response = _console.GetTextFromUser(hasFailed);
 
                 if (!_fileService.FileExists(response))
                 {
-                    _consoleRetriever.WriteTextForUser("Please select a valid file. File does not exist.");
+                    _console.WriteTextForUser("Please select a valid file. File does not exist.");
                     response = string.Empty;
                     hasFailed = true;
                 }
@@ -43,14 +43,14 @@ namespace GardnerCsvParser.Services
 
             while (!success)
             {
-                _consoleRetriever.WriteTextForUser("Please type the character that separates the rows (eg. ';' or '\\n'");
-                var result = _consoleRetriever.GetTextFromUser(hasFailed);
+                _console.WriteTextForUser("Please type the character that separates the rows (eg. ';' or '\\n'");
+                var result = _console.GetTextFromUser(hasFailed);
 
                 success = char.TryParse(result, out response);
 
                 if (!success)
                 {
-                    _consoleRetriever.WriteTextForUser("Not a valid character. Please try again.");
+                    _console.WriteTextForUser("Not a valid character. Please try again.");
                     hasFailed = true;
                 }
             }
@@ -66,15 +66,15 @@ namespace GardnerCsvParser.Services
 
             while (!success)
             {
-                _consoleRetriever.WriteTextForUser($"The amount of properties that will be mapped are not equal to the amount of properties in the headers line of the CSV file. Properties to be mapped: {propertyCount}; Properties in header: {headerCount}. Would you like to continue? y/N");
+                _console.WriteTextForUser($"The amount of properties that will be mapped are not equal to the amount of properties in the headers line of the CSV file. Properties to be mapped: {propertyCount}; Properties in header: {headerCount}. Would you like to continue? y/N");
 
-                var result = _consoleRetriever.GetTextFromUser(hasFailed);
+                var result = _console.GetTextFromUser(hasFailed);
 
                 response = ParseContinueResponse(result);
 
                 if (response is null)
                 {
-                    _consoleRetriever.WriteTextForUser("Not a valid response. Please try again.");
+                    _console.WriteTextForUser("Not a valid response. Please try again.");
                     hasFailed = true;
                     continue;
                 }
